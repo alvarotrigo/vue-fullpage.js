3ca8f27d976abad20b76ff58ac648b075c2967b8
Raphael Owino <ralphowino@gmail.com>
2018-07-16 17:57:25 +0300

    add jquery

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'fullpage.js';
+import  jQuery from 'jquery';
 import FullPage from './FullPage.vue'
 
 function plugin (Vue) {
@@ -8,6 +9,10 @@ function plugin (Vue) {
 // Install by default if using the script tag
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(plugin)
+  if(!window.$)
+  {
+    window.$ = jQuery
+  }
 }
 
 export default plugin
