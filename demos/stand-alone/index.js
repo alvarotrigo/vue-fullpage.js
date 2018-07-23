c414724ea66686255827cd3e4348bb6ccbf41113
Alvaro Trigo López <atrigol@gmail.com>
2018-07-23 18:45:58 +0200

    - Fixed stand-alone demo for IE 11

--- demos/stand-alone/index.js
+++ demos/stand-alone/index.js
@@ -1,7 +1,7 @@
 window.pepe = new Vue({
   el: '#app',
   name: 'app',
-  data () {
+  data: function(){
     return {
       options: {
         afterLoad: this.afterLoad,
@@ -14,32 +14,32 @@ window.pepe = new Vue({
     }
   },
   methods: {
-    afterLoad () {
+    afterLoad: function() {
       console.log('After load');
     },
-    addSection (e) {
-      e.preventDefault()
-      var newSectionNumber = document.querySelectorAll('.fp-section').length + 1
+    addSection: function(e) {
+      e.preventDefault();
+      var newSectionNumber = document.querySelectorAll('.fp-section').length + 1;
 
       // creating the section div
-      var section = document.createElement('div')
-      section.className = 'section'
-      section.innerHTML = `<h3>Section ${newSectionNumber}</h3>`
+      var section = document.createElement('div');
+      section.className = 'section';
+      section.innerHTML = '<h3>Section' + newSectionNumber + '</h3>';
 
       // adding section
-      document.querySelector('#fullpage').appendChild(section)
+      document.querySelector('#fullpage').appendChild(section);
 
       // creating the section menu element
-      var sectionMenuItem = document.createElement('li')
-      sectionMenuItem.setAttribute('data-menuanchor', 'page' + newSectionNumber)
-      sectionMenuItem.innerHTML = `<a href="#page${newSectionNumber}">Section${newSectionNumber}</a>`
+      var sectionMenuItem = document.createElement('li');
+      sectionMenuItem.setAttribute('data-menuanchor', 'page' + newSectionNumber);
+      sectionMenuItem.innerHTML = '<a href="#page${newSectionNumber}">Section ' + newSectionNumber + '</a>';
 
       // adding it to the sections menu
       var sectionsMenuItems = document.querySelector('#menu')
       sectionsMenuItems.appendChild(sectionMenuItem)
 
       // adding anchor for the section
-      this.options.anchors.push(`page${newSectionNumber}`)
+      this.options.anchors.push('page' + newSectionNumber)
 
       // we have to call `update` manually as DOM changes won't fire updates
       // requires the use of the attribute ref="fullpage" on the
@@ -47,7 +47,7 @@ window.pepe = new Vue({
       // ideally, use an ID element for that element too
       this.$refs.fullpage.build()
     },
-    removeSection () {
+    removeSection: function() {
       var sections = document.querySelector('#fullpage').querySelectorAll('.fp-section')
       var lastSection = sections[sections.length - 1]
 
@@ -62,10 +62,10 @@ window.pepe = new Vue({
       var lastItem = sectionsMenuItems[sectionsMenuItems.length - 1]
       lastItem.parentNode.removeChild(lastItem)
     },
-    toggleNavigation () {
+    toggleNavigation: function() {
       this.options.navigation = !this.options.navigation
     },
-    toggleScrollbar () {
+    toggleScrollbar: function() {
       this.options.scrollBar = !this.options.scrollBar
     }
   }
