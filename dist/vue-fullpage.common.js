dd83468f2e8eccc2113dcb938f4060590a08b788
Alvaro Trigo López <atrigol@gmail.com>
2019-04-10 19:57:01 +0200

    no message

--- dist/vue-fullpage.common.js
+++ dist/vue-fullpage.common.js
@@ -1,5 +1,5 @@
 /*!
- * vue-fullpage.js v0.1.2
+ * vue-fullpage.js v0.1.3
  * https://github.com/alvarotrigo/vue-fullpage.js
  *
  * @license vue-fullpage is released under the MIT license but notice
