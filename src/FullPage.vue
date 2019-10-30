a88a12ccfd2feac1f84c05443bb6e0c8dbbc44ea
dobromir-hristov <dobromir92@gmail.com>
2019-10-30 20:04:13 +0200

    feat: allow init to be delayed

--- src/FullPage.vue
+++ src/FullPage.vue
@@ -5,71 +5,72 @@
 </template>
 
 <script>
-  import fullpage from 'fullpage.js/dist/fullpage.extensions.min';
-  import * as constants from './constants';
+  import fullpage from 'fullpage.js/dist/fullpage.extensions.min'
+import * as constants from './constants'
 
-  function camelToKebab(string) {
-    return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-  }
+function camelToKebab (string) {
+    return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+}
 
   export default {
     methods: {
-      build() {
-        var slideSelector = this.options.slideSelector || '.slide';
-        var sectionSelector = this.options.sectionSelector || '.section';
-        let activeSectionIndex = fp_utils.index(document.querySelector(sectionSelector + '.active'));
-        let activeSlide = document.querySelector(sectionSelector + '.active ' + slideSelector + '.active');
-        let activeSlideIndex = activeSlide ? fp_utils.index(activeSlide) : -1;
+      build () {
+        var slideSelector = this.options.slideSelector || '.slide'
+        var sectionSelector = this.options.sectionSelector || '.section'
+        const activeSectionIndex = fp_utils.index(document.querySelector(sectionSelector + '.active'))
+        const activeSlide = document.querySelector(sectionSelector + '.active ' + slideSelector + '.active')
+        const activeSlideIndex = activeSlide ? fp_utils.index(activeSlide) : -1
 
-        this.destroy();
+        this.destroy()
 
         if (activeSectionIndex > -1) {
-          fp_utils.addClass(document.querySelectorAll(sectionSelector)[activeSectionIndex], 'active');
+          fp_utils.addClass(document.querySelectorAll(sectionSelector)[activeSectionIndex], 'active')
         }
 
         if (activeSlideIndex > -1) {
-          fp_utils.addClass(activeSlide, 'active');
+          fp_utils.addClass(activeSlide, 'active')
         }
 
-        this.init();
+        this.init()
       },
-      destroy() {
+      destroy () {
         if (typeof fullpage_api !== 'undefined' && typeof fullpage_api.destroy !== 'undefined') {
-          fullpage_api.destroy('all');
+          fullpage_api.destroy('all')
         }
       },
-      emitEvent(name, args) {
+      emitEvent (name, args) {
         // Emit event on Vue way
-        this.$emit.apply(this, [camelToKebab(name), ...args]);
+        this.$emit(camelToKebab(name), ...args)
 
-        // Run event's handler with non Vue way
+      // Run event's handler with non Vue way
         if (this.options.hasOwnProperty(name)) {
-          this.options[name].apply(this, args);
+          this.options[name].apply(this, args)
         }
       },
-      init() {
-        this.api = new fullpage(this.$refs.fullpage, this.options);
-      },
-    },
-    mounted() {
-      this.init();
-    },
-
-    beforeDestroy() {
-      if(typeof this.api !== 'undefined'){
-        this.destroy();
+      init () {
+        // eslint-disable-next-line
+        this.api = new fullpage(this.$refs.fullpage, this.options)
       }
     },
-    data(){
+    mounted () {
+      !this.delayInit && this.init()
+    },
+
+    beforeDestroy () {
+      if (typeof this.api !== 'undefined') {
+        this.destroy()
+      }
+    },
+    data () {
       return {
         events: constants.EVENTS.reduce((eventsHandlers, event) => {
           return {
             ...eventsHandlers,
 
             [event]: (...args) => {
-              this.emitEvent(event, args);
+              this.emitEvent(event, args)
             }
-          };
+          }
         }, {}),
         api: undefined
       }
@@ -79,11 +80,15 @@
         type: Object,
         required: true
       },
+      delayInit: {
+        type: Boolean,
+        default: false
+      }
     },
     watch: {
       options: {
         deep: true,
-        handler() {
+        handler () {
           this.build()
         }
       }
