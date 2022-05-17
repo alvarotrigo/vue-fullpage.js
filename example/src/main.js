a545693fba55b9e22153b08dcaf080641a4b19ed
Jonathan Schneider <jonathan@jott.media>
2022-05-17 17:52:18 +0200

    update example

--- example/src/main.js
+++ example/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'vue-fullpage.js/dist/style.css'
-import VueFullPage from 'vue-fullpage.js/dist/vue-fullpage.es'
+import VueFullPage from 'vue-fullpage.js'
 
 const app = createApp(App)
 app.use(VueFullPage)
